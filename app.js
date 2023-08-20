8f4b64f69525e8c416a8002c3ebe8888586460ef
FlagmanAlex <flagman25@mail.ru>
2023-08-20 21:07:28 +0300

    Полностью переделан модуль под канал. Изменен захват события из группы на канал

--- app.js
+++ app.js
@@ -21,39 +21,49 @@ const bot = new TelegramBot(config.get("BOT_TOKEN"), {polling: true})
 //Отлавливаем оишибку polling
 bot.on("polling_error", err => console.log(err.data.error.message));
 
-bot.on('message', async msg => {
-        Users.findOne({userId: msg.from.id}).then(u => {
-            if (!u) {
-                user.userId = msg.from.id
-                user.userTelegramName = msg.from.username
-                user.userName = msg.from.first_name
-                user.save().catch(e => console.log(e))
-            }
-        }).catch(e => console.log(e))
+bot.on('channel_post', async msg => {
+    const date = Date(msg.date)
+    // console.log(post.PostDate.toLocaleDateString('ru-RU', {
+        //     day: 'numeric',
+        //     month: 'numeric',
+        //     year: 'numeric', 
+        //     hour: 'numeric',
+        //     minute: 'numeric'
+        // }));
+    // Users.findOne({userId: msg.from.id}).then(u => {
+        //     if (!u) {
+            //         user.userId = msg.from.id
+            //         user.userTelegramName = msg.from.username
+            //         user.userName = msg.from.first_name
+            //         user.save().catch(e => console.log(e))
+            //     }
+            // }).catch(e => console.log(e))
     if (msg.photo) {
+        post.date = date
         const photo = msg.photo
         const fileId = photo[msg.photo.length-1].file_id
         let captionText = ''
         link = await bot.getFileLink(fileId)
-        post.PostId = msg.message_id
-        post.PostFileId = fileId
-        post.PostChatId = msg.chat.id
-        post.PostPhotoUrl = link
-        post.PostPhotoPath = await saveImage(link, post.PostChatId + '_' + post.PostId)
+        post.id = msg.message_id
+        post.fileId = fileId
+        post.chatId = msg.chat.id
+        post.photoUrl = link
+        post.photoPath = await saveImage(link, post.chatId + '_' + post.id)
         if (msg.caption) {
+            post.caption = msg.caption
             captionText = msg.caption
             if (msg.caption_entities)
             {
-                post.PostCaptionEntities = msg.caption_entities
-                post.PostCaption = msg.caption
+                post.captionEntities = msg.caption_entities
                 captionText = parseTelegramMessage(msg)
 
             }
-        } else post.PostCaption = ''
+        } else post.caption = ''
         post.save()
-            .catch(e => console.log(e))
-            .then(() => {
-                bot.sendPhoto(config.get('SAVE_CHAT_ID'), post.PostFileId, {caption: captionText, parse_mode: 'HTML'})
-            })
+        .catch(e => console.log(e))
+        .then(() => {
+            bot.sendMessage(config.get('FLAGMANALEX_CHAT_ID'), `Размещенный на канале ${msg.chat.title} <a href="https://t.me/c/${post.chatId.replace("-100", "")}/${post.id}">пост №${post.id}</a> сохранен в базе`, {parse_mode: 'HTML'})
+            //bot.sendPhoto(config.get('TEST_CHAT_ID'), post.PostFileId, {caption: captionText, parse_mode: 'HTML'})
+        })
     }
 })
--- models/posts.model.js
+++ models/posts.model.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose')
 const PostsSchema = new mongoose.Schema({
-    PostChatId: {type: String, required: true},
-    PostChatName: {type: String},
-    PostId: {type: String, required: true},
-    PostFileId: {type: String, required: true},
-    PostPhotoUrl: {type: String, required: true},
-    PostPhotoPath: {type: String},
-    PostCaption: {type: String},
-    PostCaptionEntities: {type: mongoose.Schema.Types.Mixed}
+    date: {type: Date} ,
+    chatId: {type: String, required: true},
+    chatName: {type: String},
+    id: {type: String, required: true},
+    fileId: {type: String, required: true},
+    photoUrl: {type: String, required: true},
+    photoPath: {type: String},
+    caption: {type: String},
+    captionEntities: {type: mongoose.Schema.Types.Mixed}
     // PostDate: {type: Date}
 })
 module.exports = mongoose.model('Posts', PostsSchema)
